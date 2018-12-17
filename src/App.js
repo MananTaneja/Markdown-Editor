f7479cdeb33ee805b01789eddb2c4ddc49958d40
Manan <manantaneja007@gmail.com>
2018-12-17 23:16:26 +0530

    Beta v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Remarkable from 'remarkable';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+      this.state = {
+        value: 'Type some *Markdown* here!'
+      };
+    }
+
+  handleChange(e) {
+    this.setState({ 
+        value: e.target.value
+    });
+  }
+
+  getRawMarkup() {
+    const md = new Remarkable();
+    return {__html: md.render(this.state.value)};
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    return(
+      <div className="container">
+        <div className="input">
+          <h3>Input</h3>
+          <textarea
+            className="input-text"
+            onChange={this.handleChange}
+            defaultValue={this.state.value}
+          />
+        </div>
+        <div className="output">
+          <h3>Markdown</h3>
+          <div
+            dangerouslySetInnerHTML={this.getRawMarkup()}
+            className="output-text">
+          </div>
+        </div>
       </div>
     );
   }
